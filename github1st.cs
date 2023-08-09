559e5483cab409906db15ffe92418562ff940d89
alikarral <141839412+alikarral@users.noreply.github.com>
2023-08-09 14:45:09 +0100

    Add files via upload

---
+++ github1st.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace first_project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What Is Your NAME");
+            string name = Console.ReadLine();
+            Console.WriteLine("hello" + name);
+
+            Console.WriteLine("How much candies do you want?");
+            string can = Console.ReadLine();
+            Console.WriteLine("You will have more 10 candies + can + 10");
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
